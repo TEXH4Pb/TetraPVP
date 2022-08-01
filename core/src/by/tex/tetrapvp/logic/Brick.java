4725390dc7210b58759ba9163c1497df607e1286
TEXH4Pb <9digger9@gmail.com>
2022-08-01 23:10:59 +0300

    Create Brick.java

---
+++ core/src/by/tex/tetrapvp/logic/Brick.java
@@ -0,0 +1,76 @@
+package by.tex.tetrapvp.logic;
+
+/**
+ * A simple 1x1 cell-brick. Every {@link by.tex.tetrapvp.logic.shapes.Shape} consists of 4 of them.
+ */
+public class Brick {
+    enum Color{
+        RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, PURPLE;
+    }
+
+    private Color color;
+    /**
+     * Horizontal position on grid. From left to right.
+     */
+    private int x;
+    /**
+     * Vertical position on grid. From bottom to top.
+     */
+    private int y;
+    /**
+     * False while brick is still a part of a moving shape.
+     */
+    private boolean laid;
+
+    public Brick(int x, int y, Color color)
+    {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        laid = false;
+    }
+
+    /**
+     * Use it when {@link by.tex.tetrapvp.logic.shapes.Shape} reaches the bottom.
+     */
+    public void stop()
+    {
+        laid = true;
+    }
+
+    public void moveDown()
+    {
+        y--;
+    }
+
+    public void moveLeft()
+    {
+        x--;
+    }
+
+    public void moveRight()
+    {
+        x++;
+    }
+
+    public boolean isLaid()
+    {
+        return laid;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
